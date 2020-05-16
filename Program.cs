bf4ccd1a9b04a4682fc0673df75263793ae85ba9
Francis Starr <38301914+francisstarr@users.noreply.github.com>
2020-05-16 11:30:16 -0400

    first_commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketClientStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipaddr = IPAddress.Parse(Server.ipAddr);
+            try {
+                client.Connect(Server.ipAddr, Server.portNum);
+                Console.ReadKey();
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.ToString());
+            }
+            
+            
+    
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocketClientStarter
+{
+    public class Server
+    {
+        public static int portNum = 23000;
+        public static String ipAddr = "192.168.0.2";
+    }
+}

2d3bcb6826dc1cccd47946ff9ce92d20bf49b49e
francisstarr <francisstarr@cmail.carleton.ca>
2020-05-16 21:25:14 -0400

    error_handling_for_offline
    Error handling added if client console is launched while server console is offline

--- Program.cs
+++ Program.cs
@@ -41,9 +41,13 @@ namespace SocketClientStarter
             }
             finally
             {
-                client.Shutdown(SocketShutdown.Both);
-                client.Close();
-                client.Dispose();
+                if (client != null){
+                    if (client.Connected) {
+                        client.Shutdown(SocketShutdown.Both);
+                    }
+                    client.Close();
+                    client.Dispose();
+                }
             }
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();

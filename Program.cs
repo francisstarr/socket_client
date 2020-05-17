771a4c91f38b77865e7773a74f037923037f5b1f
Francis Starr <38301914+francisstarr@users.noreply.github.com>
2020-05-16 20:01:42 -0400

    lesson16
    What I did after following lesson 16

--- Program.cs
+++ Program.cs
@@ -16,13 +16,36 @@ namespace SocketClientStarter
             IPAddress ipaddr = IPAddress.Parse(Server.ipAddr);
             try {
                 client.Connect(Server.ipAddr, Server.portNum);
-                Console.ReadKey();
+                Console.WriteLine("COnnection to server is a success!");
+                Console.WriteLine("Socket= " + Server.ipAddr + ":"+ Server.portNum);
+                Console.WriteLine("Type text you want to send to the server. The <ENTER>");
+                string inputCommand = string.Empty;
+                while (true)
+                {
+                    inputCommand = Console.ReadLine();
+                    if (inputCommand.Equals("<EXIT>"))
+                    {
+                        break;
+                    }
+                }
+                byte[] buffSend = Encoding.ASCII.GetBytes(inputCommand);
+                client.Send(buffSend);
+
+                byte[] buffReceived = new byte[128];
+                int nRecv = client.Receive(buffReceived);
+                Console.WriteLine("Data received: {0}", Encoding.ASCII.GetString(buffReceived, 0, nRecv));
             }
             catch (Exception e) {
                 Console.WriteLine(e.ToString());
             }
-            
-            
+            finally
+            {
+                client.Shutdown(SocketShutdown.Both);
+                client.Close();
+                client.Dispose();
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
     
         }
     }
